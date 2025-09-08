59d07e0e6c38bb0243af167e8267503eb000d822
Pranav katiyar <pranav.katiyar@decimal.co.in>
2025-09-08 21:05:56 +0530

    fixing cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
 import express from "express";
 import AuthRoutes from "./routes/auth.route.js";
-
+import cors from "cors";
 const app = express();
+
+app.use(cors({
+  origin: "localhost:1234",
+  credentials: true
+}))
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
